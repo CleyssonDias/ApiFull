a0a2ac31d8c77ca27bb7a3e2ce20dc53b894bcdd
CleyssonDias <cleysson.diass@gmail.com>
2022-02-23 17:04:56 -0300

    TypescriptSetup

---
+++ src/controllers/UserContoller.ts
@@ -0,0 +1,8 @@
+import { User } from "@models/User";
+
+export class UserContoller {
+    handle() {
+        const user = new User();
+        return "ok"
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { UserContoller } from "@controllers/UserContoller";
+import express from 'express';
+const app = express();
+const m = new UserContoller().handle()
+
+app.get('/', (req, res) => {
+    res.json({message: m})
+})
+
+app.listen(4444)
